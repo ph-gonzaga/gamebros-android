19280c26bbb9ac3558ca915d71485998e9e9bea4
João Vitor Barbosa Souza <jvbs98@hotmail.com>
2022-01-27 16:27:39 -0300

    feat(): rotas dinamica produtos categorias

--- gamebros/app/src/main/java/br/senac/gamebros/views/products/ListProductViewModel.kt
+++ gamebros/app/src/main/java/br/senac/gamebros/views/products/ListProductViewModel.kt
@@ -8,12 +8,12 @@ import br.senac.gamebros.repository.Repository
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
-class ListProductViewModel(val repository: Repository) : ViewModel() {
+class ListProductViewModel(val repository: Repository, categoryId: Int) : ViewModel() {
     val myResponse = MutableLiveData<Response<List<Product>>>()
 
-    fun listarProdutos() {
+    fun listarProdutos(categoryId: Int) {
         viewModelScope.launch {
-            val response = repository.listarProdutos()
+            val response = repository.listarProdutos(categoryId)
             myResponse.value = response
         }
     }
